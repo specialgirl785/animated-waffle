22c66672448dd99fbd7c01f59a3cf438c496dc58
specialgirl785 <118490981+specialgirl785@users.noreply.github.com>
2023-08-15 15:23:21 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+noseX = 0;
+noseY = 0;
+
+function preload(){
+    mustache = loadImage('https://i.postimg.cc/3x3QzSGq/m.png')
+}
+
+function setup(){
+    canvas = createCanvas(300,300)
+    canvas.center()
+    video = createCapture(VIDEO)
+    video.hide()
+    var posenet = ml5.poseNet(video, modelLoaded)
+     posenet.on('pose', gotPoses)
+}
+function draw(){
+    image(video,0,0,300,300)
+    image(mustache, noseX, noseY, 150, 30)
+}
+
+function save(){
+    console.log("save")
+}
+
+function modelLoaded(){
+console.log("PoseNet model has been loaded")
+}
+
+function gotPoses(results){
+    noseX = results[0].pose.nose.x;
+    noseY = results[0].pose.nose.y;
+    console.log("noseX", noseX)
+    console.log("noseY", noseY)
+} 
